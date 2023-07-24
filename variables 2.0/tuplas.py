86bec299735b8bad19d2652407033eb46512d6c2
calderonmatias <maticaldeproxd56@gmail.com>
2023-07-24 15:33:09 -0300

    24/7

---
+++ variables 2.0/tuplas.py
@@ -0,0 +1,23 @@
+
+#simplemente poniendo los valores con comas ya se crea una tupla
+#si es un solo dato siempre se pone coma al final porque si no se convierte en un simple string
+
+
+tupla = "mati","calderon"
+
+tupla = "mati",
+      
+tupla = tuple(["mati",16,"calderon"]) 
+
+#las tuplas las usamos cuando son solo datos de lectuira     
+
+
+
+
+
+
+
+
+
+
+
